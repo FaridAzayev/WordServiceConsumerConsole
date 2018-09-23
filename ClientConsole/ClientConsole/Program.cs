2de109cfc1fdc3a7fa09cbc8027e24ee39fd44cc
farid.azayev <farid.azayev@hotmail.com>
2018-09-23 12:42:41 +0400

    initial

---
+++ ClientConsole/ClientConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.Write("enter the input text file path");
+            var path = Console.ReadLine();
+            var soapClient = new WordService.WebServiceSoapClient();
+            //c:\users\farid.azayev\source\repos\ClientConsole\ClientConsole\input.txt
+            System.IO.File.ReadAllText(path).Split(' ').ToList()
+                .ForEach((w)=> Console.WriteLine(soapClient.ReverseWord(w)));
+        }
+    }
+}
